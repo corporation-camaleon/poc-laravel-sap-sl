6af9b68364a7b11421fada23194553fb12284cdf
Henry Díaz <hdiaz@realsoft.dev>
2023-03-22 12:02:34 -0600

    Se configuro el constructor base para que al levantar un requerimiento ya este creada la session necesaria para SAP

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,8 +5,25 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use RealSoft\SAPBOSL\SAPClient;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    /**
+     * Variable containing the SAP connection instance through the Service Layer.
+     */
+    protected $client = null;
+
+    /**
+     * Contructor that generates the connection to SAP using the Service Layer,
+     * which generates a SessioID of the configuration data and returns it to us
+     * by means of a variable
+     */
+    private function __contruct(){
+        $this->client = SAPClient::createSession(config('username'), config('password'), config('company_db'));
+        $session = $this->client->getSession();
+        $this->client = new SAPClient(config('SAP'), $session);
+    }
 }
